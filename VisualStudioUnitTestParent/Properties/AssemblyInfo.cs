0f36610e73a48e2034e56e69969346f2d4aa7144
tmaierhofer <tmaierhofer@localhost>
2014-02-20 09:13:36 +0100

    Update Solutions

--- VisualStudioUnitTestParent/Properties/AssemblyInfo.cs
+++ VisualStudioUnitTestParent/Properties/AssemblyInfo.cs
@@ -40,5 +40,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.5164.14639")]
-[assembly: AssemblyFileVersion("1.1.5164.14639")]
+[assembly: AssemblyVersion("1.1.5164.14806")]
+[assembly: AssemblyFileVersion("1.1.5164.14806")]
